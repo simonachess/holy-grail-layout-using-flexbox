eb9376d9b3e9ddb929fa36a4eb3f036d33b7c3f4
simonachess <simonachess@gmail.com>
2021-12-02 10:46:48 +0200

    grid layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>HEADER</header>
+        <main>MAIN CONTENT</main>
+        <aside className="left-sidebar">LEFT SIDEBAR</aside>
+        <aside className="right-sidebar">RIGHT SIDEBAR</aside>
+        <footer>FOOTER</footer>
+      </div>
     </div>
   );
 }
